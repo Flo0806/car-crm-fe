631bf1593192861de7dca37092b035c3c3c00f9d
Florian Heuberger <fh@flogersoft.de>
2024-10-19 23:01:28 +0200

    changed Customer interface

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -3,3 +3,22 @@ export enum SnackbarType {
   SUCCESS = "success",
   ERROR = "error",
 }
+
+export interface Customer {
+  id: string; // ID des Kunden
+  intNr: string; // Interne Kundennummer (z.B. "K-0001")
+  type: "DEALER" | "COMPANY" | "PRIVATE"; // Kundentyp
+  companyName: string | null; // Firmenname, falls vorhanden
+  country: string; // Land
+  zip: string; // Postleitzahl
+  city: string; // Stadt
+  street: string; // Straße und Hausnummer
+  email: string | null; // Email-Adresse der Firma
+  phone: string | null; // Telefonnummer der Firma
+  fax: string | null; // Faxnummer der Firma
+  firstName: string | null; // Vorname des Ansprechpartners
+  lastName: string | null; // Nachname des Ansprechpartners
+  contactEmail: string | null; // E-Mail des Ansprechpartners
+  contactPhone: string | null; // Telefonnummer des Ansprechpartners
+  birthDate: string | null; // Geburtsdatum des Ansprechpartners (YYYY-MM-DD)
+}
